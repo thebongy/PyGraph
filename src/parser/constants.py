b9f05db7b1c27107c5e1d90c179efdb50fe2c9fe
Rishit Bansal <rishit.bansal0@gmail.com>
2017-11-07 00:01:36 +0530

    Added some basic math constants

--- src/parser/constants.py
+++ src/parser/constants.py
@@ -1,3 +1,14 @@
 '''
 File to store special mathematical constants supported by the program
 '''
+
+import math
+
+CONSTANTS = {
+        "e":math.e,
+        "pi":math.pi,
+        "tau": math.tau
+        }
+
+def add_constant(name, value):
+    CONSTANTS[name] = value
