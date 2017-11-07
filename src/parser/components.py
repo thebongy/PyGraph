97bad09739473f187fc8bb1529921addfae1b618
Rishit Bansal <rishit.bansal0@gmail.com>
2017-11-07 22:40:14 +0530

    Removed class repetitions, reordered

--- src/parser/components.py
+++ src/parser/components.py
@@ -4,6 +4,22 @@
 Classes required to construct an expression using post-fix
 '''
 
+class Unit(object):
+    '''
+    Represents an arbitrary, most basic distinctive component of an expression.
+    '''
+    def __init__(self, data, start, end):
+        self.raw_data = data
+        self.start = start
+        self.end = end
+
+class Constant(Unit):
+    def __init__(self, data, start, end):
+        Unit.__init__(self, data, start, end)
+        self.value = float(data)
+    def evaluate(self, x):
+        return self.value
+
 class ObjectRepresentation(object):
     '''
     Converts a string expression into a list of python objects.
@@ -65,15 +81,6 @@ class ObjectRepresentation(object):
                     #end2 = len(data1)+len(stuff)+1 ignore
                     TestL.append(Function(data1, start1, end1, Dictoffns[data1], stuff))
                     fnvar = True
-
-class Unit(object):
-    '''
-    Represents an arbitrary, most basic distinctive component of an expression.
-    '''
-    def __init__(self, data, start, end):
-        self.raw_data = data
-        self.start = start
-        self.end = end
 
 class Parenthesis(Unit):
     '''
@@ -198,95 +205,7 @@ class PostfixExpression(object):
                 arg = evaluation.pop()
                 evaluation.push(current.evaluate(arg))
         return evaluation.pop()
-class ObjectRepresentation(object):
-    '''
-    Converts a string expression into a list of python objects.
-    '''
-    def __init__(self, expr):
-        self.string_repr = expr
-        self.expression = []
-        self.construct()
 
-    def construct(self):
-        #new
-        TestL = []
-        bcnt = 0
-        Blist = []
-        Lofconst = []                                                                                              
-        Dictoffns ={}
-        fnvar = True
-        DictofUOp = {}
-        DictofBOp = {}
-        for i in range(self.string_repr):
-            if fnvar:
-                if self.string_repr[i] == '(':
-                    TestL.append('Dummy')
-                    bcnt += 1
-                    Blist.append(len(TestL)-1)
-                    
-                if self.string_repr[i] == ')':
-                    posn = Blist.pop(-1)
-                    TestL[posn] = Parenthesis('(',posn,len(TestL),bcnt)
-                    TestL.append(Parenthesis(')',posn,len(TestL),bcnt))
-                    bcnt -= 1
-                    
-
-                if self.string_repr[i] == 'x':
-                    TestL.append(Variable('x',i,i+1))                                                                     #                         
-
-                for j in DictofUOp:
-                    if self.string_repr[i] == j and (self.string_repr[i] in ['('] and self.string_repr[i] in DictofBOp) :
-                        TestL.append(UnaryOperator(j, i, i+1, DictofUOp[j][0], DictofUOp[j][1]))                         #change5
-
-                for j in DictofBOp:
-                    if self.string_repr[i] == j:
-                        TestL.append(BinaryOperator(j, i, i+1, DictofUOp[j][0], DictofUOp[j][1]))
-                        
-
-                for j in Dictoffns:
-                    if self.string_repr[i:(i+len(j))] == j:
-                        data1 = j                                                                                 
-                        start1 = i
-                        fnvar = False
-
-                #afterfn do const
-                if self.string_repr[i] in Lofconst:
-                    TestL.append(Constant(self.string_repr[i],len(TestL),len(TestL)+1))
-            if not fnvar:
-                if i == ')':
-                    stuff = self.string_repr[start1+len(data1)+1:i]
-                    end1 = i
-                    #end2 = len(data1)+len(stuff)+1 ignore
-                    TestL.append(Function(data1, start1, end1, Dictoffns[data1], stuff))
-                    fnvar = True
-
-class Unit(object):
-    '''
-    Represents an arbitrary, most basic distinctive component of an expression.
-    '''
-    def __init__(self, data, start, end):
-        self.raw_data = data
-        self.start = start
-        self.end = end
-
-class Parenthesis(Unit):
-    '''
-    Represents individual opening brackets '(' and closing brackets ')' components in the expression.
-    '''
-    def __init__(self, char, start, end, Pdepth):
-        Unit.__init__(self,char,start,end)
-        
-        # PDepth: (Example):
-        #
-        # Expression:        ( x + ( x * ( x / 4 ) ) )
-        #                    ^     ^     ^       ^ ^ ^
-        # PDepth Value:      0     1     2       2 1 0
-        
-        if char == "(":
-            self.type = "OPENING"
-        else:
-            self.type = "CLOSING"
-        self.depth = Pdepth
 class Expression(object):
     '''
     Class to represent any arbitrary string math expression in postfix notation.
@@ -305,7 +224,7 @@ class Operator(Unit):
         Unit.__init__(self, data, start, end, func, priority)
         self.func = func
         self.priority = priority
-    def evaluate(*args):
+    def evaluate(self):
         raise NotImplementedError
 
 
@@ -330,13 +249,6 @@ class Variable(Unit):
     def evaluate(self, x):
         return x
 
-
-class Constant(Unit):
-    def __init__(self, data, start, end, func):
-        Unit.__init__(self, data, start, end)
-        self.value = float(data)
-    def evaluate(self, x):
-        return self.value
 
 class Function(Unit):
     def __init__(self, data, start, end, func, stuffinside):
